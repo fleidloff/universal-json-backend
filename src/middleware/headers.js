47f828050a6621a5aa0af3fcf085d211ed39db6d
Frederik Leidloff <kontakt@frederik-leidloff.de>
2020-10-05 13:17:40 +0200

    add content-type to allow headers

--- src/middleware/headers.js
+++ src/middleware/headers.js
@@ -1,6 +1,9 @@
 export default function headers(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "x-user,x-secret,x-password");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "x-user,x-secret,x-password,content-type"
+  );
   return next();
 }
